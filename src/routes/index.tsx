22a8223a9b738d4ba7bd251409efef8ad6dffdd5
Alexander Zakharov <alexander.zakharov@aldra.no>
2024-01-26 21:22:28 +0100

    feat: show header with avatar and logo on main page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,16 +1,28 @@
-import { component$, useStylesScoped$ } from '@builder.io/qwik';
+import { $, component$, useStylesScoped$ } from '@builder.io/qwik';
 import { type DocumentHead } from '@builder.io/qwik-city';
 
+import Avatar from '@project/components/avatar';
+import Header from '@project/components/header';
 import Logo from '@project/components/logo';
 import { Theme } from '@project/enums';
+import { useAuthenticatedUser } from '@project/hooks';
+import { auth } from '@project/utils/auth';
 
 import styles from './styles.css?inline';
 
 export default component$(() => {
   useStylesScoped$(styles);
 
+  const signIn = $(() => auth().signIn());
+
+  const { user, signOut } = useAuthenticatedUser(signIn);
+
   return (
     <main id='page'>
+      <Header>
+        <Logo theme={Theme.LIGHT} height='100%' q:slot='logo' />
+        <Avatar title={user.name} onClick$={signOut} q:slot='avatar' />
+      </Header>
       <Logo theme={Theme.LIGHT} height={70} />
       <h1>Aldra Platform kommer snart! 🚀</h1>
     </main>
