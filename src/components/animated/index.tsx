29f813f72014861ad205d80601897b02feb94784
Alexander Zakharov <alexander.zakharov@aldra.no>
2024-01-28 16:27:03 +0100

    feat: implement animated component

---
+++ src/components/animated/index.tsx
@@ -0,0 +1,48 @@
+import { component$, useStylesScoped$, Slot } from '@builder.io/qwik';
+
+import styles from './styles.css?inline';
+
+interface Props {
+  animation: 'fade-in-up' | 'zoom-pop-in';
+  delay?:
+    | '0.1s'
+    | '0.2s'
+    | '0.3s'
+    | '0.4s'
+    | '0.5s'
+    | '0.6s'
+    | '0.7s'
+    | '0.8s'
+    | '0.9s'
+    | '1s'
+    | '1.1s'
+    | '1.2s'
+    | '1.3s'
+    | '1.4s'
+    | '1.5s'
+    | '1.6s'
+    | '1.7s'
+    | '1.8s'
+    | '1.9s'
+    | '2s'
+    | '2.1s'
+    | '2.2s'
+    | '2.3s'
+    | '2.4s'
+    | '2.5s'
+    | '2.6s'
+    | '2.7s'
+    | '2.8s'
+    | '2.9s'
+    | '3s';
+}
+
+export default component$<Props>(({ animation, delay }) => {
+  useStylesScoped$(styles);
+
+  return (
+    <div data-animation={animation} data-delay={delay}>
+      <Slot />
+    </div>
+  );
+});
