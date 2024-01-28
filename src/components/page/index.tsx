0c3c809f0baee4aa51f2f2b60b83f969081165ee
Alexander Zakharov <alexander.zakharov@aldra.no>
2024-01-28 20:39:46 +0100

    feat: implement page component

---
+++ src/components/page/index.tsx
@@ -0,0 +1,42 @@
+import {
+  $,
+  component$,
+  useStylesScoped$,
+  Slot,
+  type HTMLAttributes,
+} from '@builder.io/qwik';
+
+import { Theme } from '@project/enums';
+import { useAuthenticatedUser } from '@project/hooks';
+import { auth } from '@project/utils/auth';
+
+import Avatar from '../avatar';
+import Header from '../header';
+import Logo from '../logo';
+
+import styles from './styles.css?inline';
+
+interface Props extends HTMLAttributes<HTMLElement> {}
+
+export default component$<Props>(({ ...props }) => {
+  useStylesScoped$(styles);
+
+  const signIn = $(() => auth().signIn());
+
+  const { user, signOut } = useAuthenticatedUser(signIn);
+
+  return (
+    <main {...props}>
+      <Header>
+        <Logo theme={Theme.LIGHT} height='100%' q:slot='logo' />
+        <Avatar title={user.name} onClick$={signOut} q:slot='avatar' />
+      </Header>
+      <div data-slot='breadcrumbs'>
+        <Slot name='breadcrumbs' />
+      </div>
+      <div data-slot='content'>
+        <Slot />
+      </div>
+    </main>
+  );
+});
