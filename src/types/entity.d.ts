b9feb359f338aef6b78bd21c2f1441d150e2fb8d
Alexander Zakharov <alexander.zakharov@aldra.no>
2024-02-15 18:34:50 +0100

    chore: add Bookmark type

--- src/types/entity.d.ts
+++ src/types/entity.d.ts
@@ -3,9 +3,12 @@ import { type Identifiable, type Nameable } from './common';
 export namespace Entity {
   export interface Client extends Identifiable, Nameable {}
 
+  export interface Bookmark extends Identifiable {}
+
   export interface Assignment extends Identifiable {
     client: Client;
     role: string;
     status: 'active' | 'concluded' | 'cancelled';
+    bookmark?: Bookmark;
   }
 }
