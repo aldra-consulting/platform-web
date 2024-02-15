1fc9bed68ad7d7868fc7b5abf469a949a660f1a9
Alexander Zakharov <alexander.zakharov@aldra.no>
2024-02-15 18:34:20 +0100

    chore: add Assignment type

--- src/types/entity.d.ts
+++ src/types/entity.d.ts
@@ -2,4 +2,10 @@ import { type Identifiable, type Nameable } from './common';
 
 export namespace Entity {
   export interface Client extends Identifiable, Nameable {}
+
+  export interface Assignment extends Identifiable {
+    client: Client;
+    role: string;
+    status: 'active' | 'concluded' | 'cancelled';
+  }
 }
