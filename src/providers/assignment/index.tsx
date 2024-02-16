786db69972095416735d0ace07e8c004cee09325
Alexander Zakharov <alexander.zakharov@aldra.no>
2024-02-16 09:06:37 +0100

    feat: implement assignment provider

---
+++ src/providers/assignment/index.tsx
@@ -0,0 +1,36 @@
+import {
+  $,
+  component$,
+  useContextProvider,
+  useStore,
+  Slot,
+} from '@builder.io/qwik';
+
+import { AssignmentContext, type AssignmentStore } from '@project/context';
+import { AssignmentService } from '@project/services';
+import { type Entity } from '@project/types';
+
+export interface Props {
+  assignment: Entity.Assignment;
+}
+
+const service = new AssignmentService();
+
+export default component$<Props>(({ assignment }) => {
+  useContextProvider(
+    AssignmentContext,
+    useStore<AssignmentStore>({
+      assignment,
+      toggleBookmark: $(async function run(this: AssignmentStore) {
+        try {
+          this.assignment.bookmark =
+            (await service.toggleBookmark(this.assignment.id)) ?? undefined;
+        } catch (error) {
+          // TODO: handle errors
+        }
+      }),
+    })
+  );
+
+  return <Slot />;
+});
---
+++ src/providers/index.ts
@@ -0,0 +1,1 @@
+export { default as AssignmentProvider } from './assignment';
