52738be545ef13af8957c2a10967a4865234f09f
Alexander Zakharov <alexander.zakharov@aldra.no>
2024-02-17 14:29:49 +0100

    refactor: use switch statement to render assignment header status

--- src/routes/assignments/components/assignment/components/header/components/status/index.tsx
+++ src/routes/assignments/components/assignment/components/header/components/status/index.tsx
@@ -1,4 +1,4 @@
-import { component$, useComputed$, useContext } from '@builder.io/qwik';
+import { component$, useContext } from '@builder.io/qwik';
 
 import Chip from '@project/components/chip';
 import { AssignmentContext } from '@project/context';
@@ -8,35 +8,26 @@ export default component$(() => {
     assignment: { status },
   } = useContext(AssignmentContext);
 
-  const color = useComputed$(() => {
-    switch (status) {
-      case 'active':
-        return 'blue';
-      case 'concluded':
-        return 'neutral';
-      case 'cancelled':
-        return 'red';
-      default:
-        return undefined;
-    }
-  });
-
-  const translation = useComputed$(() => {
-    switch (status) {
-      case 'active':
-        return 'Aktiv';
-      case 'concluded':
-        return 'Avsluttet';
-      case 'cancelled':
-        return 'Avlyst';
-      default:
-        return 'Ukjent';
-    }
-  });
-
-  return (
-    <Chip variant='outlined' color={color.value}>
-      {translation.value}
-    </Chip>
-  );
+  switch (status) {
+    case 'active':
+      return (
+        <Chip variant='outlined' size='small' color='blue'>
+          Aktiv
+        </Chip>
+      );
+    case 'concluded':
+      return (
+        <Chip variant='outlined' size='small' color='neutral'>
+          Avsluttet
+        </Chip>
+      );
+    case 'cancelled':
+      return (
+        <Chip variant='outlined' size='small' color='red'>
+          Avlyst
+        </Chip>
+      );
+    default:
+      return null;
+  }
 });
