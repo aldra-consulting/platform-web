858e51a08885ef5e622d8c6144b77b4d0c774a46
Alexander Zakharov <alexander.zakharov@aldra.no>
2024-02-17 22:42:02 +0100

    feat: extend Assignment model with a roles attribute

--- src/types/entity.d.ts
+++ src/types/entity.d.ts
@@ -7,9 +7,17 @@ export namespace Entity {
 
   export interface Bookmark extends Identifiable {}
 
+  export interface Applicant extends Identifiable {}
+
+  export interface Role extends Identifiable, Nameable {
+    status: 'open' | 'review' | 'filled';
+    applicant?: Applicant;
+  }
+
   export interface Assignment extends Identifiable, Nameable {
     client: Client;
     status: 'active' | 'concluded' | 'cancelled';
+    roles: Role[];
     brief?: string;
     description?: string;
     bookmark?: Bookmark;
