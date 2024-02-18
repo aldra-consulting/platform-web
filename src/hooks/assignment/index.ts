f23e2edf291011783a2a5f818468223ee30ade48
Alexander Zakharov <alexander.zakharov@aldra.no>
2024-02-18 11:35:18 +0100

    feat: refactor useAssignment hook

--- src/hooks/assignment/index.ts
+++ src/hooks/assignment/index.ts
@@ -1,24 +1,9 @@
-import { useResource$ } from '@builder.io/qwik';
+import { type QRL, type ValueOrPromise } from '@builder.io/qwik';
 
-import { type Assignment } from '@project/types';
+import { type Entity, type Nullable } from '@project/types';
 
-export const useAssignment = (id: string) => {
-  const resource = useResource$<Assignment | null>(async () => {
-    try {
-      return Promise.resolve({
-        id: `assignment-${id}`,
-        client: {
-          id: `client-${id}`,
-          name: `Client ${id}`,
-        },
-        role: `Role ${id}`,
-      });
-    } catch (error) {
-      return null;
-    }
-  });
+import { useReloadableResource } from '../reloadable-resource';
 
-  return {
-    resource,
-  } as const;
-};
+export const useAssignment = (
+  resource: QRL<() => ValueOrPromise<Nullable<Entity.Assignment>>>
+) => useReloadableResource(resource);
