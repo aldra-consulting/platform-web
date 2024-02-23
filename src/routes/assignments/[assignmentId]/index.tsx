7018d54618f212aa336a0fe87a8c6eb555c9e2b6
Alexander Zakharov <alexander.zakharov@aldra.no>
2024-02-23 09:51:05 +0100

    fix: move data loading out of layout

--- src/routes/assignments/[assignmentId]/index.tsx
+++ src/routes/assignments/[assignmentId]/index.tsx
@@ -1,5 +1,6 @@
-import { component$, useContext } from '@builder.io/qwik';
+import { $, component$, Resource } from '@builder.io/qwik';
 import {
+  useLocation,
   type DocumentHead,
   type StaticGenerateHandler,
 } from '@builder.io/qwik-city';
@@ -7,41 +8,56 @@ import {
 import Breadcrumbs from '@project/components/breadcrumbs';
 import Link from '@project/components/link';
 import Page from '@project/components/page';
-import { AssignmentContext } from '@project/context';
+import Redirect from '@project/components/redirect';
+import { useAssignment } from '@project/hooks';
+import { AssignmentProvider } from '@project/providers';
 import { AssignmentService } from '@project/services';
 
 import Assignment from './components/assignment';
 
 export default component$(() => {
-  const {
-    assignment: { name },
-  } = useContext(AssignmentContext);
+  const { params } = useLocation();
+
+  const { resource } = useAssignment(
+    $(() => new AssignmentService().findByIdOrThrow(params.assignmentId ?? ''))
+  );
 
   return (
-    <Page>
-      <Breadcrumbs q:slot='breadcrumbs'>
-        <Breadcrumbs.Breadcrumb>
-          <Link href='https://www.aldra.no' color='neutral'>
-            Aldra
-          </Link>
-        </Breadcrumbs.Breadcrumb>
-        <Breadcrumbs.Separator>/</Breadcrumbs.Separator>
-        <Breadcrumbs.Breadcrumb>
-          <Link href='/' color='neutral'>
-            Plattform
-          </Link>
-        </Breadcrumbs.Breadcrumb>
-        <Breadcrumbs.Separator>/</Breadcrumbs.Separator>
-        <Breadcrumbs.Breadcrumb>
-          <Link href='/assignments' color='neutral'>
-            Oppdrag
-          </Link>
-        </Breadcrumbs.Breadcrumb>
-        <Breadcrumbs.Separator>/</Breadcrumbs.Separator>
-        <Breadcrumbs.Breadcrumb active>{name}</Breadcrumbs.Breadcrumb>
-      </Breadcrumbs>
-      <Assignment />
-    </Page>
+    <Resource
+      value={resource}
+      onPending={() => null}
+      onRejected={() => <Redirect to='/assignments' />}
+      onResolved={(assignment) => (
+        <AssignmentProvider assignment={assignment}>
+          <Page>
+            <Breadcrumbs q:slot='breadcrumbs'>
+              <Breadcrumbs.Breadcrumb>
+                <Link href='https://www.aldra.no' color='neutral'>
+                  Aldra
+                </Link>
+              </Breadcrumbs.Breadcrumb>
+              <Breadcrumbs.Separator>/</Breadcrumbs.Separator>
+              <Breadcrumbs.Breadcrumb>
+                <Link href='/' color='neutral'>
+                  Plattform
+                </Link>
+              </Breadcrumbs.Breadcrumb>
+              <Breadcrumbs.Separator>/</Breadcrumbs.Separator>
+              <Breadcrumbs.Breadcrumb>
+                <Link href='/assignments' color='neutral'>
+                  Oppdrag
+                </Link>
+              </Breadcrumbs.Breadcrumb>
+              <Breadcrumbs.Separator>/</Breadcrumbs.Separator>
+              <Breadcrumbs.Breadcrumb active>
+                {assignment.name}
+              </Breadcrumbs.Breadcrumb>
+            </Breadcrumbs>
+            <Assignment />
+          </Page>
+        </AssignmentProvider>
+      )}
+    />
   );
 });
 
--- src/routes/assignments/[assignmentId]/layout.tsx
+++ src/routes/assignments/[assignmentId]/layout.tsx
@@ -1,28 +1,3 @@
-import { $, component$, Resource, Slot } from '@builder.io/qwik';
-import { useLocation } from '@builder.io/qwik-city';
+import { component$, Slot } from '@builder.io/qwik';
 
-import Redirect from '@project/components/redirect';
-import { useAssignment } from '@project/hooks';
-import { AssignmentProvider } from '@project/providers';
-import { AssignmentService } from '@project/services';
-
-export default component$(() => {
-  const { params } = useLocation();
-
-  const { resource } = useAssignment(
-    $(() => new AssignmentService().findByIdOrThrow(params.assignmentId ?? ''))
-  );
-
-  return (
-    <Resource
-      value={resource}
-      onPending={() => null}
-      onRejected={() => <Redirect to='/assignments' />}
-      onResolved={(assignment) => (
-        <AssignmentProvider assignment={assignment}>
-          <Slot />
-        </AssignmentProvider>
-      )}
-    />
-  );
-});
+export default component$(() => <Slot />);
