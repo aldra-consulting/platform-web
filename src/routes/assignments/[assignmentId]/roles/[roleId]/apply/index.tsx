4d37a911f2eac7e4129e5e729052962b64ab51ba
Alexander Zakharov <alexander.zakharov@aldra.no>
2024-02-29 16:22:22 +0100

    feat: use client resource to load assignment and role

--- src/routes/assignments/[assignmentId]/roles/[roleId]/apply/index.tsx
+++ src/routes/assignments/[assignmentId]/roles/[roleId]/apply/index.tsx
@@ -1,57 +1,89 @@
-import { component$, useContext } from '@builder.io/qwik';
+import { $, component$ } from '@builder.io/qwik';
 import {
   type StaticGenerateHandler,
   type DocumentHead,
 } from '@builder.io/qwik-city';
 
 import Breadcrumbs from '@project/components/breadcrumbs';
+import ClientResource from '@project/components/client-resource';
 import Link from '@project/components/link';
 import Page from '@project/components/page';
-import { RoleContext } from '@project/context';
-import { useAssignmentContext } from '@project/hooks';
+import Redirect from '@project/components/redirect';
+import { useAssignmentId, useClientResource, useRoleId } from '@project/hooks';
+import { AssignmentProvider } from '@project/providers';
 import { AssignmentService } from '@project/services';
 
 export default component$(() => {
-  const { assignment } = useAssignmentContext();
-  const { role } = useContext(RoleContext);
+  const assignmentId = useAssignmentId();
+  const roleId = useRoleId();
+
+  const resource = useClientResource(
+    $(() =>
+      new AssignmentService()
+        .findByIdOrThrow(assignmentId)
+        .then((assignment) => {
+          const role = assignment.roles.find(({ id }) => id === roleId);
+
+          if (!role) {
+            throw new Error('Role not found');
+          }
+
+          return { assignment, role };
+        })
+    )
+  );
 
   return (
-    <Page>
-      <Breadcrumbs q:slot='breadcrumbs'>
-        <Breadcrumbs.Breadcrumb>
-          <Link href='https://www.aldra.no' color='neutral'>
-            Aldra
-          </Link>
-        </Breadcrumbs.Breadcrumb>
-        <Breadcrumbs.Separator>/</Breadcrumbs.Separator>
-        <Breadcrumbs.Breadcrumb>
-          <Link href='/' color='neutral'>
-            Plattform
-          </Link>
-        </Breadcrumbs.Breadcrumb>
-        <Breadcrumbs.Separator>/</Breadcrumbs.Separator>
-        <Breadcrumbs.Breadcrumb>
-          <Link href='/assignments' color='neutral'>
-            Oppdrag
-          </Link>
-        </Breadcrumbs.Breadcrumb>
-        <Breadcrumbs.Separator>/</Breadcrumbs.Separator>
-        <Breadcrumbs.Breadcrumb>
-          <Link href={`/assignments/${assignment.id}`} color='neutral'>
-            {assignment.name}
-          </Link>
-        </Breadcrumbs.Breadcrumb>
-        <Breadcrumbs.Separator>/</Breadcrumbs.Separator>
-        <Breadcrumbs.Breadcrumb>
-          <Link href={`/assignments/${assignment.id}/roles`} color='neutral'>
-            Roller
-          </Link>
-        </Breadcrumbs.Breadcrumb>
-        <Breadcrumbs.Separator>/</Breadcrumbs.Separator>
-        <Breadcrumbs.Breadcrumb active>{role.name}</Breadcrumbs.Breadcrumb>
-      </Breadcrumbs>
-      <pre>{JSON.stringify(role, null, 2)}</pre>
-    </Page>
+    <ClientResource
+      resource={resource}
+      onPending={() => null}
+      onRejected={() => <Redirect to={`/assignments/${assignmentId}`} />}
+      onResolved={({ assignment, role }) => (
+        <AssignmentProvider assignment={assignment}>
+          <Page>
+            <Breadcrumbs q:slot='breadcrumbs'>
+              <Breadcrumbs.Breadcrumb>
+                <Link href='https://www.aldra.no' color='neutral'>
+                  Aldra
+                </Link>
+              </Breadcrumbs.Breadcrumb>
+              <Breadcrumbs.Separator>/</Breadcrumbs.Separator>
+              <Breadcrumbs.Breadcrumb>
+                <Link href='/' color='neutral'>
+                  Plattform
+                </Link>
+              </Breadcrumbs.Breadcrumb>
+              <Breadcrumbs.Separator>/</Breadcrumbs.Separator>
+              <Breadcrumbs.Breadcrumb>
+                <Link href='/assignments' color='neutral'>
+                  Oppdrag
+                </Link>
+              </Breadcrumbs.Breadcrumb>
+              <Breadcrumbs.Separator>/</Breadcrumbs.Separator>
+              <Breadcrumbs.Breadcrumb>
+                <Link href={`/assignments/${assignment.id}`} color='neutral'>
+                  {assignment.name}
+                </Link>
+              </Breadcrumbs.Breadcrumb>
+              <Breadcrumbs.Separator>/</Breadcrumbs.Separator>
+              <Breadcrumbs.Breadcrumb>
+                <Link
+                  href={`/assignments/${assignment.id}/roles`}
+                  color='neutral'
+                >
+                  Roller
+                </Link>
+              </Breadcrumbs.Breadcrumb>
+              <Breadcrumbs.Separator>/</Breadcrumbs.Separator>
+              <Breadcrumbs.Breadcrumb active>
+                {role.name}
+              </Breadcrumbs.Breadcrumb>
+            </Breadcrumbs>
+            <pre>{JSON.stringify(role, null, 2)}</pre>
+          </Page>
+        </AssignmentProvider>
+      )}
+    />
   );
 });
 
