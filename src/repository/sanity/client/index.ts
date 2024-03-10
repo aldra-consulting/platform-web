5f363c4a00265efa6b8a8c4ffd8b788252dc5f57
Alexander Zakharov <alexander.zakharov@aldra.no>
2024-03-10 19:12:50 +0100

    chore: format GROQ queries

--- src/repository/sanity/client/index.ts
+++ src/repository/sanity/client/index.ts
@@ -11,7 +11,12 @@ export default class ClientSanityRepository extends SanityRepository<Sanity.Docu
 
   findMany = async (): Promise<Sanity.Document.Client[]> => {
     try {
-      const query = groq`*[_type == $type]{ ..., 'id': _id }`;
+      const query = groq`
+        *[_type == $type] {
+          ...,
+          "id": _id
+        }
+      `;
 
       return await this.client.fetch<Sanity.Document.Client[]>(query, {
         type: this.type,
@@ -26,7 +31,12 @@ export default class ClientSanityRepository extends SanityRepository<Sanity.Docu
   findByIdOrThrow = async (
     id: Sanity.Document.Client['id']
   ): Promise<Sanity.Document.Client> => {
-    const query = groq`*[_type == $type && _id == $id]{ ..., 'id': _id }`;
+    const query = groq`
+      *[_type == $type && _id == $id] {
+        ...,
+        "id": _id
+      }
+    `;
 
     const [document] = await this.client.fetch<[Sanity.Document.Client]>(
       query,
