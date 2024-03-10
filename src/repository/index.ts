b0c72ecc4ee1e3a2ffc138607ac4e10d9b53c7c3
Alexander Zakharov <alexander.zakharov@aldra.no>
2024-03-10 18:49:55 +0100

    feat: add Sanity Mission repository implementation

--- src/repository/index.ts
+++ src/repository/index.ts
@@ -1,1 +1,2 @@
 export { default as ClientSanityRepository } from './sanity/client';
+export { default as MissionSanityRepository } from './sanity/mission';
---
+++ src/repository/sanity/mission/index.ts
@@ -0,0 +1,45 @@
+import groq from 'groq';
+
+import { type Sanity } from '@project/types';
+import { isDefined } from '@project/utils/common';
+
+import SanityRepository from '..';
+import { SanityDocumentNotFoundError } from '../errors';
+
+export default class MissionSanityRepository extends SanityRepository<Sanity.Document.Mission> {
+  protected type = 'mission' as const;
+
+  findMany = async (): Promise<Sanity.Document.Mission[]> => {
+    try {
+      const query = groq`*[_type == $type]{ ..., 'id': _id }`;
+
+      return await this.client.fetch<Sanity.Document.Mission[]>(query, {
+        type: this.type,
+      });
+    } catch (error) {
+      throw new Error(`Unable to find documents of type '${this.type}'`, {
+        cause: error,
+      });
+    }
+  };
+
+  findByIdOrThrow = async (
+    id: Sanity.Document.Mission['id']
+  ): Promise<Sanity.Document.Mission> => {
+    const query = groq`*[_type == $type && _id == $id]{ ..., 'id': _id }`;
+
+    const [document] = await this.client.fetch<[Sanity.Document.Mission]>(
+      query,
+      { type: this.type, id }
+    );
+
+    if (!isDefined(document)) {
+      throw new SanityDocumentNotFoundError<Sanity.Document.Mission>(
+        id,
+        this.type
+      );
+    }
+
+    return document;
+  };
+}
