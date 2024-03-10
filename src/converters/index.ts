38570555a04c20600a888551efe30e7ba15b7287
Alexander Zakharov <alexander.zakharov@aldra.no>
2024-03-10 19:52:50 +0100

    feat: add Sanity Mission document to Mission entity converter

--- src/converters/index.ts
+++ src/converters/index.ts
@@ -1,1 +1,2 @@
 export { default as SanityClientDocumentToClientEntityConverter } from './sanity-client-document-to-client-entity';
+export { default as SanityMissionDocumentToMissionEntityConverter } from './sanity-mission-document-to-mission-entity';
---
+++ src/converters/sanity-mission-document-to-mission-entity/index.ts
@@ -0,0 +1,29 @@
+import { type Entity, type Converter } from '@project/types';
+import { type Sanity } from '@project/types/sanity';
+
+export default class
+  implements Converter<Sanity.Document.Mission, Entity.Mission>
+{
+  #clientConverter: Converter<Sanity.Document.Client, Entity.Client>;
+
+  constructor(
+    clientConverter: Converter<Sanity.Document.Client, Entity.Client>
+  ) {
+    this.#clientConverter = clientConverter;
+  }
+
+  convert = ({
+    _id: id,
+    label,
+    description,
+    client,
+    status,
+  }: Sanity.Document.Mission): Entity.Mission => ({
+    id,
+    label: label.find(({ _key }) => _key === 'no')?.value ?? '',
+    description: description.find(({ _key }) => _key === 'no')?.value ?? '',
+    client: this.#clientConverter.convert(client),
+    status,
+    roles: [],
+  });
+}
