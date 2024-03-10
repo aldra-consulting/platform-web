d6477fb175027f839a9b064271b444854e8f8286
Alexander Zakharov <alexander.zakharov@aldra.no>
2024-03-10 19:53:51 +0100

    feat: expand Client reference on Mission document

--- src/repository/sanity/mission/index.ts
+++ src/repository/sanity/mission/index.ts
@@ -11,7 +11,16 @@ export default class MissionSanityRepository extends SanityRepository<Sanity.Doc
 
   findMany = async (): Promise<Sanity.Document.Mission[]> => {
     try {
-      const query = groq`*[_type == $type]{ ..., 'id': _id }`;
+      const query = groq`
+        *[_type == $type] {
+          ...,
+          "id": _id,
+          client-> {
+            ...,
+            "id": _id
+          }
+        }
+      `;
 
       return await this.client.fetch<Sanity.Document.Mission[]>(query, {
         type: this.type,
@@ -26,7 +35,16 @@ export default class MissionSanityRepository extends SanityRepository<Sanity.Doc
   findByIdOrThrow = async (
     id: Sanity.Document.Mission['id']
   ): Promise<Sanity.Document.Mission> => {
-    const query = groq`*[_type == $type && _id == $id]{ ..., 'id': _id }`;
+    const query = groq`
+      *[_type == $type && _id == $id] {
+        ...,
+        "id": _id,
+        client-> {
+          ...,
+          "id": _id
+        }
+      }
+    `;
 
     const [document] = await this.client.fetch<[Sanity.Document.Mission]>(
       query,
