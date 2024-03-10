94db688b77c02c29c149e8cf9318c13d6c8804aa
Alexander Zakharov <alexander.zakharov@aldra.no>
2024-03-10 17:55:51 +0100

    chore: remove unused hook

--- src/hooks/assignment/index.ts
+++
@@ -1,9 +0,0 @@
-import { type QRL, type ValueOrPromise } from '@builder.io/qwik';
-
-import { type Entity } from '@project/types';
-
-import { useReloadableResource } from '../reloadable-resource';
-
-export const useAssignment = (
-  resource: QRL<() => ValueOrPromise<Entity.Assignment>>
-) => useReloadableResource(resource);
--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,4 +1,3 @@
-export * from './assignment';
 export * from './assignment-context';
 export * from './assignment-id';
 export * from './assignments';
