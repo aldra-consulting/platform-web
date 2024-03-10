dc658e8edf269cc3e4343fb11aeefeb134ae1ada
Alexander Zakharov <alexander.zakharov@aldra.no>
2024-03-10 18:40:08 +0100

    chore: remove unused service

--- src/services/entity/index.ts
+++
@@ -1,7 +0,0 @@
-import { type Service } from '@project/types';
-
-export default abstract class EntityService<T> implements Service.ForEntity<T> {
-  protected abstract entity: string;
-
-  abstract findMany: () => Promise<T[]>;
-}
