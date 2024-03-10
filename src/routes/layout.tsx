5c7698d06e6a705ca6b8ce8025ad93a952792f1c
Alexander Zakharov <alexander.zakharov@aldra.no>
2024-03-10 18:21:33 +0100

    chore: add error boundary hook in global layout

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,9 +1,11 @@
-import { $, component$, Slot } from '@builder.io/qwik';
+import { $, component$, Slot, useErrorBoundary } from '@builder.io/qwik';
 
 import { useAuthenticatedUser } from '@project/hooks';
 import { auth } from '@project/utils/auth';
 
 export default component$(() => {
+  useErrorBoundary();
+
   const signIn = $(() => auth().signIn());
 
   const { isAuthenticated } = useAuthenticatedUser(signIn);
