c5537514a1db04b8e2a0f42879b077e2f10c07d4
Alexander Zakharov <alexander.zakharov@aldra.no>
2024-04-07 23:38:27 +0200

    chore: add utility type to work with unions

--- src/types/common.d.ts
+++ src/types/common.d.ts
@@ -15,6 +15,26 @@ export type DeepPartial<T> = {
   [K in keyof T]?: T[K] extends object ? DeepPartial<T[K]> : T[K];
 };
 
+export type UnionToIntersection<U> = (
+  U extends unknown ? (k: U) => void : never
+) extends (k: infer I) => void
+  ? I
+  : never;
+
+export type UnionToOverloads<U> = UnionToIntersection<
+  U extends unknown ? (f: U) => void : never
+>;
+
+export type PopUnion<U> =
+  UnionToOverloads<U> extends (a: infer A) => void ? A : never;
+
+type IsUnion<T> = [T] extends [UnionToIntersection<T>] ? false : true;
+
+export type UnionToArray<T, A extends unknown[] = []> =
+  IsUnion<T> extends true
+    ? UnionToArray<Exclude<T, PopUnion<T>>, [PopUnion<T>, ...A]>
+    : [T, ...A];
+
 export interface Identifiable<ID extends Hashable = string> {
   id: ID;
 }
