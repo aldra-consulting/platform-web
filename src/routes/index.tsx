b694972a94f3d47385c110b2ee1439de922cf72f
Alexander Zakharov <alexander.zakharov@aldra.no>
2024-04-08 00:48:43 +0200

    refactor: use 'Page' component on main route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,34 +1,19 @@
-import { $, component$, useStylesScoped$ } from '@builder.io/qwik';
+import { component$, useStylesScoped$ } from '@builder.io/qwik';
 import { type DocumentHead } from '@builder.io/qwik-city';
 
-import Avatar from '@project/components/avatar';
-import Header from '@project/components/header';
-import Logo from '@project/components/logo';
-import { Theme } from '@project/enums';
-import { useAuthenticatedUser } from '@project/hooks';
-import { auth } from '@project/utils/auth';
+import Page from '@project/components/page';
 
 import styles from './styles.css?inline';
 
 export default component$(() => {
   useStylesScoped$(styles);
 
-  const signIn = $(() => auth().signIn());
-
-  const {
-    store: { user },
-    signOut,
-  } = useAuthenticatedUser(signIn);
-
   return (
-    <main id='page'>
-      <Header>
-        <Logo theme={Theme.LIGHT} height='100%' q:slot='logo' />
-        <Avatar title={user?.name} onClick$={signOut} q:slot='avatar' />
-      </Header>
-      <Logo theme={Theme.LIGHT} height={70} />
-      <h1>Aldra Platform kommer snart! 🚀</h1>
-    </main>
+    <Page>
+      <div data-slot='content'>
+        <h1>Aldra Platform kommer snart! 🚀</h1>
+      </div>
+    </Page>
   );
 });
 
