b62b271e4d219a36842fdc8b3218e89001e681e5
Alexander Zakharov <alexander.zakharov@aldra.no>
2024-04-08 23:04:11 +0200

    feat: improve 'Loader' component

--- src/components/loader/index.tsx
+++ src/components/loader/index.tsx
@@ -4,14 +4,31 @@ import {
   useStylesScoped$,
 } from '@builder.io/qwik';
 
+import { CssDimensionToStringConverter } from '@project/converters';
+import { type CSS } from '@project/types';
+
 import styles from './styles.css?inline';
 
 interface Props extends HTMLAttributes<HTMLElement> {
   color?: 'neutral' | 'blue' | 'red' | 'yellow' | 'green';
+  thickness?: CSS.DataType.Length;
 }
 
-export default component$<Props>(({ color = 'neutral', ...props }) => {
-  useStylesScoped$(styles);
+const converter = new CssDimensionToStringConverter();
 
-  return <span {...props} data-root data-color={color} />;
-});
+export default component$<Props>(
+  ({ color = 'neutral', thickness, ...props }) => {
+    useStylesScoped$(styles);
+
+    return (
+      <span
+        {...props}
+        data-root
+        data-color={color}
+        style={{
+          '--thickness': thickness ? converter.convert(thickness) : undefined,
+        }}
+      />
+    );
+  }
+);
