d2b88155a3983ea81eeefd26ec4c0aab67b1e94c
Alexander Zakharov <alexander.zakharov@aldra.no>
2024-04-08 12:08:26 +0200

    feat: add 'variant' prop to 'Link' component

--- src/components/link/index.tsx
+++ src/components/link/index.tsx
@@ -5,18 +5,21 @@ import styles from './styles.css?inline';
 
 export interface Props extends LinkProps {
   color?: 'neutral' | 'blue' | 'red' | 'yellow' | 'green' | 'inherit';
+  variant?: 'plain' | 'underlined';
 }
 
-export default component$<Props>(({ color = 'inherit', ...props }) => {
-  useStylesScoped$(styles);
+export default component$<Props>(
+  ({ color = 'inherit', variant = 'underlined', ...props }) => {
+    useStylesScoped$(styles);
 
-  return (
-    <Link {...props}>
-      <span data-slot='content' data-color={color}>
-        <span data-slot='underlined'>
-          <Slot />
+    return (
+      <Link {...props}>
+        <span data-slot='content' data-color={color}>
+          <span data-variant={variant}>
+            <Slot />
+          </span>
         </span>
-      </span>
-    </Link>
-  );
-});
+      </Link>
+    );
+  }
+);
