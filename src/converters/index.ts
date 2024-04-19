7ff279f5cce8699f36008c0d3a937fe06ed5d0d7
Alexander Zakharov <alexander.zakharov@aldra.no>
2024-04-19 22:16:12 +0200

    feat: add OpendID Connect 'User' to 'User' entity converter

--- src/converters/index.ts
+++ src/converters/index.ts
@@ -1,4 +1,5 @@
 export { default as CssDimensionToStringConverter } from './css-dimension-to-string';
+export { default as OidcUserToUserEntityConverter } from './oidc-user-to-user-enity';
 export { default as SanityAwardCriterionObjectToAwardCriterionEntityConverter } from './sanity-award-criterion-object-to-award-criterion-entity';
 export { default as SanityClientDocumentToClientEntityConverter } from './sanity-client-document-to-client-entity';
 export { default as SanityCriterionDocumentToCriterionEntityConverter } from './sanity-criterion-document-to-criterion-entity';
---
+++ src/converters/oidc-user-to-user-enity/index.ts
@@ -0,0 +1,13 @@
+import { type User } from 'oidc-client-ts';
+
+import { type Converter, type Entity, type ID } from '@project/types';
+
+export default class implements Converter<User, Entity.User> {
+  convert = ({
+    profile: { sub: id, name = '', picture },
+  }: User): Entity.User => ({
+    id: id as ID.User,
+    name,
+    picture,
+  });
+}
